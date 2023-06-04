5a86f3c67a1fd8451a30e45aa62d42dfe5965b22
frankpagan <frank@cocreate.app>
2023-06-04 06:11:01 -0600

    fix: Refactor CoCreate.config.js to remove hard-coded credentials

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,28 +1,28 @@
 module.exports = {
     "config": {
-        "organization_id": "5ff747727005da1c272740ab",
-        "key": "2061acef-0451-4545-f754-60cf8160",
-        "host": "general.cocreate.app"
+        "organization_id": "",
+        "key": "",
+        "host": ""
     },
-    
+
     "sources": [{
-            "collection": "files",
-            "document": {
-                "_id": "639dac751fff0844144f971d",
-                "name": "index.html",
-                "path": "/docs/codearea/index.html",
-                "src": "{{./docs/index.html}}",
-                "hosts": [
-                    "cocreate.app",
-                    "general.cocreate.app"
-                ],
-                "directory": "/docs/codearea",
-                "parentDirectory": "{{parentDirectory}}",
-                "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
-            }
+        "collection": "files",
+        "document": {
+            "_id": "639dac751fff0844144f971d",
+            "name": "index.html",
+            "path": "/docs/codearea/index.html",
+            "src": "{{./docs/index.html}}",
+            "hosts": [
+                "cocreate.app",
+                "general.cocreate.app"
+            ],
+            "directory": "/docs/codearea",
+            "parentDirectory": "{{parentDirectory}}",
+            "content-type": "{{content-type}}",
+            "public": "true",
+            "website_id": "644d4bff8036fb9d1d1fd69c"
         }
+    }
     ]
 }
 
