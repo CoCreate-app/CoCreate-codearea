e6ef4eb9c649d216a3caf9fbd0435315fefb374b
frankpagan <frank@cocreate.app>
2023-09-18 17:43:27 -0600

    fix: support pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -15,10 +15,8 @@ module.exports = {
                 "general.cocreate.app"
             ],
             "directory": "codearea",
-            "parentDirectory": "{{parentDirectory}}",
             "content-type": "{{content-type}}",
-            "public": "true",
-            "website_id": "644d4bff8036fb9d1d1fd69c"
+            "public": "true"
         }
     }
     ]
