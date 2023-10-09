6e7ee5678b917c69c3a7f9bf1df9a3b37c66d7f7
frankpagan <frank@cocreate.app>
2023-10-08 21:01:21 -0600

    formating

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -2,23 +2,22 @@ module.exports = {
     "organization_id": "",
     "key": "",
     "host": "",
-
-    "sources": [{
-        "array": "files",
-        "object": {
-            "_id": "639dac751fff0844144f971d",
-            "name": "index.html",
-            "path": "/docs/codearea/index.html",
-            "src": "{{./docs/index.html}}",
-            "host": [
-                "cocreate.app",
-                "general.cocreate.app"
-            ],
-            "directory": "codearea",
-            "content-type": "{{content-type}}",
-            "public": "true"
+    "sources": [
+        {
+            "array": "files",
+            "object": {
+                "_id": "639dac751fff0844144f971d",
+                "name": "index.html",
+                "path": "/docs/codearea/index.html",
+                "src": "{{./docs/index.html}}",
+                "host": [
+                    "cocreate.app",
+                    "general.cocreate.app"
+                ],
+                "directory": "codearea",
+                "content-type": "{{content-type}}",
+                "public": "true"
+            }
         }
-    }
     ]
-}
-
+};
